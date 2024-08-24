ee180ff8bb68529a432bb951082ecab04a56277f
Collin Haynes <collinjk23@gmail.com>
2024-08-24 13:48:20 -0400

    Made the methods for the distance conversions now working on the table portion

---
+++ M01/A1/DistanceConversion.java
@@ -0,0 +1,22 @@
+public class DistanceConversion {
+    public static double footToMeter (double foot) {
+        return foot * 0.305;
+
+    }
+
+    public static double meterToFoot (double meter) {
+        return 3.279 * meter;
+    }
+
+    
+    
+    public static void main(String[] args) {
+        //Top of the table
+        System.out.println("Feet    Meters  |   Meters  Feet");
+
+        //Outline of the table
+        String firstHalf = "-".repeat(20);
+        String secondHalf = "-".repeat(20);
+        for (int)
+    }
+}
