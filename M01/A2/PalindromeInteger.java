2610883e6fce8b14838d29ac166378075bb69ef1
Collin Haynes <collinjk23@gmail.com>
2024-08-24 20:18:41 -0400

    Finished rest of the program and fixed the reverse method

--- M01/A2/PalindromeInteger.java
+++ M01/A2/PalindromeInteger.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class PalindromeInteger {
 
    //Methods for the program
@@ -8,15 +11,34 @@ public class PalindromeInteger {
             reversing = reversing * 10 + number % 10; 
             number /= 10; //removing the last digit
         }
+        return reversing;
     }
 
     public static boolean isPalindrome(int number) {
-        return number == reverse(number);
+        return number == reverse(number); 
     }
 
 
  
  public static void main(String[] args) {
+
+    Scanner userInput = new Scanner(System.in);
+//This section gets the users input
+System.out.print("Enter a number: ");
+int number = userInput.nextInt();
+
+System.out.println("The reverse of your number is: " + reverse(number));
+
+//checks if the number is a Palindrome
+if (isPalindrome(number)) {
+    System.out.println("The number you entered is a palidrome");
+    
+} else {
+    System.out.println("The number you entered is not a palidrome");
+}
+
+userInput.close();
+
 }
 
 }
