044a19ca1cd38bd5d46c68910d38ff2366d0d4bf
Collin Haynes <collinjk23@gmail.com>
2024-08-25 17:14:35 -0400

    Finished program as well as fully commented out program and listed sources

--- M01/A3/MajorDiagonal.java
+++ M01/A3/MajorDiagonal.java
@@ -1,4 +1,5 @@
-import java util.Scanner;
+
+import java.util.Scanner;
 
 public class MajorDiagonal {
 
@@ -10,15 +11,33 @@ public class MajorDiagonal {
         }
         return total;
     }
+
+    //User Input Method
+    public static double[][] getArray() {
+        Scanner userInput = new Scanner(System.in);
+        double[][] m = new double[4][4]; 
+
+    System.out.println("Enter " + m.length + " rows and " + m[0].length + " columns: ");
+            //For this portion of code I used help from listing 8.1 on page 296 in the Liang Book
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) { 
+                m[i][j] = userInput.nextDouble();
+            }
+        }   
+        return m;   
+    }
     
 
 
      public static void main(String[] args) {
 
-    java.util.Scanner userinput = new java.util.Scanner(System.in);
-    System.out.println("Enter " +)
+        double[][] matrix = getArray();
 
+        double sum = sumMajorDiagonal(matrix);
 
-}
+        //final output of what the user enters
+        System.out.println("The sum of the elements in your major diagonal is: " + sum);
+
+     }
 
 }
