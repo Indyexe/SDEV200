3a82cde7e610f8015409c753390d4079eab71482
Collin Haynes <collinjk23@gmail.com>
2024-08-25 16:02:42 -0400

    Trying to figure out the Major Diagnoal method

---
+++ M01/A3/MajorDiagonal.java
@@ -0,0 +1,26 @@
+import java util.Scanner;
+
+public class MajorDiagonal {
+
+    //sum of the major diagonal method
+    public static double sumMajorDiagonal (double [] [] m) {
+        double total = 0;
+        for (int row = 0; row < m.length; row++) {
+            for (int column = 0; column < m[row].length; column++) {
+                total += m[row][column];
+            }
+        }
+        return total;
+    }
+    
+
+
+     public static void main(String[] args) {
+
+    java.util.Scanner userinput = new java.util.Scanner(System.in);
+    System.out.println("Enter " +)
+
+
+}
+
+}
