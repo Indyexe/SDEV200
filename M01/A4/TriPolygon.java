59510d6ed21b43bc199dc57d469973cf68205c10
Collin Haynes <collinjk23@gmail.com>
2024-08-25 23:26:07 -0400

    Initialized a few of the data fields

---
+++ M01/A4/TriPolygon.java
@@ -0,0 +1,14 @@
+public class TriPolygon {
+    //data fields
+    private int n =3;
+    private double side;
+    private double x;
+    private double y;
+
+public static void main(String[] args) {
+    TriPolygon RegularPolygon = new TriPolygon(); //renaming to RegularPolygon
+    
+}
+
+
+}
