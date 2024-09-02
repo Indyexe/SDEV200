c4de51e8809711a3c9aa999315bdd48cdcd69113
Collin Haynes <collinjk23@gmail.com>
2024-09-02 00:02:04 -0400

    test

--- M02/A2/TheTriangleClass.java
+++ M02/A2/TheTriangleClass.java
@@ -1,66 +1,134 @@
+import java.util.Scanner;
+
 public class TheTriangleClass {
 
+    public abstract class GeometricObject {
+        private String color = "white";
+        private boolean filled;
 
-public abstract class GeometricObject {
-  private String color = "white";
-  private boolean filled;
+        // Default constructor
+        protected GeometricObject() {
+        }
 
-  //Default construct*/
-  protected GeometricObject() {
-  }
+        // Constructor to initialize color and filled status
+        protected GeometricObject(String color, boolean filled) {
+            this.color = color;
+            this.filled = filled;
+        }
 
-  //Construct a geometric object/
-  protected GeometricObject(String color, boolean filled) {
-    this.color = color;
-    this.filled = filled;
-  }
+        // Getter method for color
+        public String getColor() {
+            return color;
+        }
 
-  /**Getter method for color/
-  public String getColor() {
-    return color;
-  }
+        // Setter method for color
+        public void setColor(String color) {
+            this.color = color;
+        }
 
-  /Setter method for color*/
-  public void setColor(String color) {
-    this.color = color;
-  }
+        // Getter method for filled status
+        public boolean isFilled() {
+            return filled;
+        }
 
-  //Getter method for filled. Since filled is boolean,
-     //so, the get method name is isFilled
-  public boolean isFilled() {
-    return filled;
-  }
+        // Setter method for filled status
+        public void setFilled(boolean filled) {
+            this.filled = filled;
+        }
 
-  /**Setter method for filled/
-  public void setFilled(boolean filled) {
-    this.filled = filled;
-  }
+        // Abstract method to find the area
+        public abstract double getArea();
 
-  /Abstract method findArea*/
-  public abstract double getArea();
-
-  //Abstract method getPerimeter*/
-  public abstract double getPerimeter();
-}
-public class Triangle extends GeometricObject {
-    private double side1 = 1.0;
-    private double side2 = 1.0;
-    private double side3 = 1.0;
-
-    public Triangle() {
-
+        // Abstract method to get the perimeter
+        public abstract double getPerimeter();
     }
 
-    public Triangle(double side1, double side2, double side3) {
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
+    // Extending the GeometricObject
+    public class Triangle extends GeometricObject {
+        private double side1 = 1.0;
+        private double side2 = 1.0;
+        private double side3 = 1.0;
+
+        public Triangle() {
+        }
+
+        public Triangle(double side1, double side2, double side3) {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+
+        // Initializing sides, color, and filled status
+        public Triangle(double side1, double side2, double side3, String shade, boolean isfilled) {
+            super(shade, isfilled);
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+
+        public double getFirstSide() {
+            return side1;
+        }
+
+        public double getSecondSide() {
+            return side2;
+        }
+
+        public double getThirdSide() {
+            return side3;
+        }
+
+        // Implements the abstract method to calculate the area
+        @Override
+        public double getArea() {
+            double angleC = Math.acos((side1 * side1 + side2 * side2 - side3 * side3) / (2 * side1 * side2));
+            return 0.5 * side1 * side2 * Math.sin(angleC);
+        }
+
+        // Implements the abstract method to calculate the perimeter
+        @Override
+        public double getPerimeter() {
+            return side1 + side2 + side3;
+        }
+
+        // This String describes the triangle
+        @Override
+        public String toString() {
+            return "Triangle Details: " +
+            "\nSide 1: " + side1 + 
+            "\nSide 2: " + side2 +
+            "\nSide 3: " + side3 +
+            "\nTotal Perimeter: " + getPerimeter() +
+            "\nArea: " + getArea();
+        }
     }
 
-}
+    // Main method to test the Triangle class
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        // Outputs that prompt users to enter information about triangle
+        System.out.println("Please enter the length for the first side of the Triangle: ");
+        double side1 = input.nextDouble();
+        System.out.println("Please enter the length for the second side of the Triangle: ");
+        double side2 = input.nextDouble();
+        System.out.println("Please enter the length for the third side of the Triangle: ");
+        double side3 = input.nextDouble();
 
+        // Asks user to enter in the color of the triangle
+        System.out.println("Please enter the color of the Triangle: ");
+        String color = input.next();
 
+        System.out.println("Is the triangle filled? Please input True or False: ");
+        boolean filled = input.nextBoolean();
 
+        TheTriangleClass outer = new TheTriangleClass();
+        Triangle triangle = outer.new Triangle(side1, side2, side3, color, filled);
 
+        // This displays the properties of the triangle
+        System.out.println("\nThis is what your Triangle is detailed of:");
+        System.out.println(triangle);
+
+        input.close();
+    }
 }
